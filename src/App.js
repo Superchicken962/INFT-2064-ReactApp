de15747856b51bc26e37f51ce7dcd56509bcfb4b
Superchicken962 <58424347+Superchicken962@users.noreply.github.com>
2025-10-28 11:53:51 +1030

    Move audio and effect controls into separate components

--- src/App.js
+++ src/App.js
@@ -9,6 +9,8 @@ import { getAudioContext, webaudioOutput, registerSynthSounds } from '@strudel/w
 import { registerSoundfonts } from '@strudel/soundfonts';
 import { stranger_tune } from './tunes';
 import console_monkey_patch, { getD3Data } from './console-monkey-patch';
+import AudioControls from './components/AudioControls';
+import EffectControls from './components/EffectControls';
 
 let globalEditor = null;
 
@@ -31,16 +33,6 @@ export function SetupButtons() {
         }
     }
     )
-}
-
-
-
-export function ProcAndPlay() {
-    if (globalEditor != null && globalEditor.repl.state.started == true) {
-        console.log(globalEditor)
-        Proc()
-        globalEditor.evaluate();
-    }
 }
 
 export function Proc() {
@@ -118,14 +110,7 @@ return (
                         <textarea className="form-control" rows="15" id="proc" ></textarea>
                     </div>
                     <div className="col-md-4">
-
-                        <nav>
-                            <button id="process" className="btn btn-outline-primary">Preprocess</button>
-                            <button id="process_play" className="btn btn-outline-primary">Proc & Play</button>
-                            <br />
-                            <button id="play" className="btn btn-outline-primary">Play</button>
-                            <button id="stop" className="btn btn-outline-primary">Stop</button>
-                        </nav>
+                        <AudioControls />
                     </div>
                 </div>
                 <div className="row">
@@ -134,18 +119,7 @@ return (
                         <div id="output" />
                     </div>
                     <div className="col-md-4">
-                        <div className="form-check">
-                            <input className="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault1" onChange={ProcAndPlay} defaultChecked />
-                            <label className="form-check-label" htmlFor="flexRadioDefault1">
-                                p1: ON
-                            </label>
-                        </div>
-                        <div className="form-check">
-                            <input className="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault2" onChange={ProcAndPlay} />
-                            <label className="form-check-label" htmlFor="flexRadioDefault2">
-                                p1: HUSH
-                            </label>
-                        </div>
+                        <EffectControls strudel={ globalEditor } />
                     </div>
                 </div>
             </div>
