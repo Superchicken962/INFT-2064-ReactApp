12d384f070fed5f999dd8225fca9f4ef79632191
Superchicken962 <58424347+Superchicken962@users.noreply.github.com>
2025-10-28 17:20:58 +1030

    Add template components for dial input & mixer sliders

---
+++ src/components/input/MixerSlider.jsx
@@ -0,0 +1,11 @@
+// A slider - like a range input, but bigger and in fashion of a dj slider.
+
+const MixerSlider = () => {
+    return (
+        <>
+            <p>A mixer slider</p>
+        </>
+    );
+}
+
+export default MixerSlider;
