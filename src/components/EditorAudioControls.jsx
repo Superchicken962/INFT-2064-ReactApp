b11cce9cce4e30e5395b75581420c0a0735ba3e4
Superchicken962 <58424347+Superchicken962@users.noreply.github.com>
2025-11-05 00:58:53 +1030

    Change play and stop button to sit on the same line

--- src/components/EditorAudioControls.jsx
+++ src/components/EditorAudioControls.jsx
@@ -13,29 +13,12 @@ const EditorAudioControls = ({ volumeKey }) => {
 
     return (
         <>
-            {/* <nav>
-                <button id="process" className="btn btn-outline-primary" onClick={processAudio}>Preprocess</button>
-                <button id="process_play" className="btn btn-outline-primary" onClick={procPlayAudio}>Proc & Play</button>
-                <br />
-
-                <div className="row justify-content-left">
-                    <div className="col-md-2">
-                        <button id="play" className="btn btn-outline-success" onClick={playAudio}>Play</button>
-                    </div>
-
-                    <div className="col-md-2">
-                        <button id="stop" className="btn btn-outline-danger" onClick={stopAudio}>Stop</button>
-                    </div>
-
-                    <div className="col-md-6">
-                        <input id="volume" name={ volumeKey } className="form-range" type="range" value={ volume } min="0" max="100" step="1" onChange={ changeVolume } />
-                    </div>
-                </div>
-            </nav> */}
-
             <button className="btn btn-outline-primary d-block w-100 mb-2" type="button" onClick={processAudio}>Preprocess</button>
-            <button className="btn btn-outline-success d-block w-100 mb-2" type="button" onClick={playAudio}>Play</button>
-            <button className="btn btn-outline-danger d-block w-100 mb-2" type="button" onClick={stopAudio}>Stop</button>
+
+            <div className="d-block">
+                <button className="btn btn-outline-success d-inline-block mb-2" style={{width: "48%"}} type="button" onClick={playAudio}>Play</button>
+                <button className="btn btn-outline-danger d-inline-block mb-2 ms-2" style={{width: "48%"}} type="button" onClick={stopAudio}>Stop</button>  
+            </div>
 
             <div className="volumeContainer">
                 <FontAwesomeIcon icon={faVolumeDown} />
